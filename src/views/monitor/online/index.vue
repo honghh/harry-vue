adb457e6d5b637f8bc5af66f0048d4d7d775a962
hh hong <183865800@qq.com>
2020-03-10 17:35:39 +0800

    使用高级表格属性

--- src/views/monitor/online/index.vue
+++ src/views/monitor/online/index.vue
@@ -1,55 +1,71 @@
 <template>
   <div class="app-container">
     <div class="head-container" />
-    <!--表格渲染-->
-    <vxe-table
-      border
-      stripe
+    <vxe-grid
       resizable
-      highlight-hover-row
-      height="500"
+      export-config
+      border
+      height="680"
       :loading="listLoading"
-      :checkbox-config="{labelField: 'id', highlight: true, range: true}"
+      :pager-config="tablePage"
+      :form-config="tableForm"
+      :toolbar="tableToolbar"
       :data="list"
-    >
-      <vxe-table-column type="seq" width="60" />
-      <vxe-table-column type="checkbox" title="ID" width="140" />
-      <vxe-table-column field="username" title="用户名" sortable />
-      <vxe-table-column field="nickName" title="用户昵称" show-overflow />
-      <vxe-table-column field="job" title="工作" show-overflow />
-      <vxe-table-column field="ip" title="登陆IP" show-overflow />
-      <vxe-table-column field="address" title="登陆地点" show-overflow />
-      <vxe-table-column field="browser" title="浏览器" show-overflow />
-      <vxe-table-column field="loginTime" title="登陆时间" show-overflow />
-    </vxe-table>
-    <!--分页组件-->
-    <vxe-pager
-      perfect
-      size="mini"
-      :loading="listLoading"
-      :current-page="listQuery.pageNum"
-      :page-size="listQuery.pageSize"
-      :total="total"
-      :layouts="['PrevPage', 'JumpNumber', 'NextPage', 'FullJump', 'Sizes', 'Total']"
-      @page-change="handlePageChange"
+      :columns="tableColumn"
+      @form-submit="getList"
+      @page-change="pageChangeEvent"
     />
   </div>
 </template>
 
 <script>
 import { fetchList } from '@/api/monitor-online'
+
 const defaultListQuery = {
   pageNum: 1,
-  pageSize: 5,
-  keyword: null
+  pageSize: 10,
+  keyword: ''
 }
+
 export default {
   data() {
     return {
       listLoading: false,
+      tablePage: {
+        total: 0,
+        currentPage: 1,
+        pageSize: 10,
+        pageSizes: [10, 20, 50, 100],
+        align: 'left',
+        layouts: ['Sizes', 'PrevJump', 'PrevPage', 'Number', 'NextPage', 'NextJump', 'FullJump', 'Total'],
+        perfect: true
+      },
       listQuery: Object.assign({}, defaultListQuery),
-      list: null,
-      total: 0
+      list: [],
+      tableForm: {
+        data: {
+          keyword: ''
+        },
+        items: [
+          { field: 'keyword', title: '关键字 ', itemRender: { name: '$input', props: { placeholder: '请输入关键字' }}},
+          { itemRender: { name: '$buttons', children: [{ props: { type: 'submit', content: '查询', status: 'primary' }}, { props: { type: 'reset', content: '重置' }}] }}
+        ]
+      },
+      tableToolbar: {
+        export: true,
+        custom: true
+      },
+      tableColumn: [
+        { type: 'seq', width: 60 },
+        { type: 'checkbox', title: 'ID', width: 60 },
+        { field: 'username', title: '用户名', minWidth: 120, remoteSort: true },
+        { field: 'nickName', title: '用户昵称', minWidth: 120, remoteSort: true },
+        { field: 'job', title: '工作', minWidth: 120, remoteSort: true },
+        { field: 'ip', title: '登陆IP', minWidth: 120, remoteSort: true },
+        { field: 'address', title: '登陆地点', minWidth: 120, remoteSort: true },
+        { field: 'browser', title: '浏览器', minWidth: 120, remoteSort: true },
+        { field: 'loginTime', title: '登陆时间', minWidth: 120, remoteSort: true }
+      ]
     }
   },
   created() {
@@ -58,17 +74,21 @@ export default {
   methods: {
     getList() {
       this.listLoading = true
+      this.listQuery.pageNum = this.tablePage.currentPage
+      this.listQuery.pageSize = this.tablePage.pageSize
+      this.listQuery.keyword = this.tableForm.data.keyword
+
       fetchList(this.listQuery).then(response => {
         this.list = response.data.list
-        this.total = response.data.total
+        this.tablePage.total = response.data.total
         setTimeout(() => {
           this.listLoading = false
         }, 0.3 * 1000)
       })
     },
-    handlePageChange({ currentPage, pageSize }) {
-      this.listQuery.pageNum = currentPage
-      this.listQuery.pageSize = pageSize
+    pageChangeEvent({ currentPage, pageSize }) {
+      this.tablePage.currentPage = currentPage
+      this.tablePage.pageSize = pageSize
       this.getList()
     }
   }
